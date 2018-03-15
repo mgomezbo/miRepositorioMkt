3c01c090e5fe18ac50013f5dd2bd10706a4a1c0c
mgomezbo <37306315+mgomezbo@users.noreply.github.com>
2018-03-15 18:14:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -69,20 +69,23 @@ if ('development' == app.get('env')) {
 console.log('Definida Config');
 // HubExchange Routes
 app.get('/', routes.index );
+console.log('get ok');
 app.post('/login', tokenFromJWT, routes.login );
+console.log('login ok');
 app.post('/logout', routes.logout );
+console.log('logout ok');
 
 // Custom Activity Routes for interacting with Desk.com API
 app.post('/ixn/activities/create-case/save/', activityCreate.save );
 app.post('/ixn/activities/create-case/validate/', activityCreate.validate );
 app.post('/ixn/activities/create-case/publish/', activityCreate.publish );
 app.post('/ixn/activities/create-case/execute/', activityCreate.execute );
-
+console.log('create-case ok');
 app.post('/ixn/activities/update-case/save/', activityUpdate.save );
 app.post('/ixn/activities/update-case/validate/', activityUpdate.validate );
 app.post('/ixn/activities/update-case/publish/', activityUpdate.publish );
 app.post('/ixn/activities/update-case/execute/', activityUpdate.execute );
-
+console.log('update-case ok');
 app.get('/clearList', function( req, res ) {
 	// The client makes this request to get the data
 	activityUtils.logExecuteData = [];
